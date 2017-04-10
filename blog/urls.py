1a1023e9334fc4bbc5fe167ff20a6bc8372bab4e
pelatiah <pelatiah.erhahon@yahoo.com>
2017-04-10 17:08:09 -0400

    After template extension modification

--- blog/urls.py
+++ blog/urls.py
@@ -3,6 +3,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^oreva/', views.oreva, name='oreva'),
-    url(r'^post/', views.post, name='post'),
+    url(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,16 +1,14 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 from . models import Post, Comment
 from django.utils import timezone
+from django.shortcuts import render, get_object_or_404
 
 
 def index(request):
     posts = Post.objects.filter(publish_date__lte=timezone.now()).order_by('publish_date')
     return render(request, 'blog/index.html', {'posts': posts})
 
-def oreva(request):
-    return HttpResponse('<h2>This is the first try with the proper Configuration</h2>')
-
-def post(request):
-    return HttpResponse('<h2>Have you ever felt	that the world is more and more	about technology to	which you cannot (yet) relate? Have you	ever</h2>')
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
 # Create your views here.
