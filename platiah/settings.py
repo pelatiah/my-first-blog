215afad3fc200354f6c31b2159e8c444ee289ba0
pelatiah <pelatiah.erhahon@yahoo.com>
2017-04-15 15:55:00 -0400

    Second try

--- platiah/settings.py
+++ platiah/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'b4y@za7xe8qovnwp9&6p=83n3la5pw8t)$e0k@1_nb*=k2ce=='
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1', '.platiah.com', '.pythonanywhere.com']
 
 
 # Application definition
